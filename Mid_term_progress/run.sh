e5950fbceb68c32441fb7af13be1ccc2ed123f97
josephanand0 <djanandkumar9@gmail.comm>
2024-10-25 20:08:41 -0600

    data commit

--- Mid_term_progress/run.sh
+++ Mid_term_progress/run.sh
@@ -3,6 +3,9 @@
 echo "For the output of Adaboost model"
 python3 adaboost.py
 
+echo "For the output of cleaned data"
+python3 datacleaning.py
+
 echo "For the output of random forest model"
 python3 randomforest.py
 
